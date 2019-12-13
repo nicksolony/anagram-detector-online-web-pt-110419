35148e33aaae03413bd99b676be0eebf60544776
Nick Solonyy <nicksolony@gmail.com>
2019-12-13 22:47:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+  
+  
+  def initialize (word)
+    
+    @word = word
+  
+  end
+
+  def match (list)             #need to check which words from the list would make an Anagram using                                original word
+    anagrams = []
+    
+    list.each {|w| w.split.sort
+      if w.split.sort == @word.split.sort
+        anagrams << w
+        
+    }
+    anagrams
+  end
+  
+end
