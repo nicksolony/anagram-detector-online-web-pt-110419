717a31a71cceee99c24dff1f0ba4f7556ad5cf4a
Nick Solonyy <nicksolony@gmail.com>
2019-12-13 22:50:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ require "pry"
 
   def match (list)             #need to check which words from the list would make an Anagram using                                original word
     anagrams = []
-    
+ binding.pry   
     list.each {|w| w.split.sort
       if w.split.sort == @word.split.sort
         anagrams << w
