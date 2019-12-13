2d988d0a877a857a4d40bb125579a8317813e42e
Nick Solonyy <nicksolony@gmail.com>
2019-12-13 22:54:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+require "pry"
+  attr_accessor :word
+  
+  
+  def initialize (word)
+    
+    @word = word
+  
+  end
+
+  def match (list)             #need to check which words from the list would make an Anagram using                                original word
+    anagrams = []
+    
+    list.each {|w| w.split.sort
+   # binding.pry
+      if w.split("").sort == @word.split("").sort
+        anagrams << w
+      end
+    }
+	anagrams
+  end
+
+end
